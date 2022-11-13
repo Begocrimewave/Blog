eda6f8809aa4a81ea29285b39081434d5701ff92
Begocm <elcorreodebgo@gmail.com>
2022-11-13 20:31:37 +0100

    Formulario component

--- src/app/components/formulario/formulario.component.ts
+++ src/app/components/formulario/formulario.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-formulario',
   templateUrl: './formulario.component.html',
-  styleUrls: ['./formulario.component.css']
+  styleUrls: ['./formulario.component.css'],
 })
 export class FormularioComponent implements OnInit {
+  formNewPost: FormGroup;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
+    this.formNewPost = new FormGroup({
+      titulo: new FormControl(''),
+      texto: new FormControl(''),
+      autor: new FormControl(''),
+      imagen: new FormControl(''),
+      fecha: new FormControl(''),
+      categoria: new FormControl(''),
+    });
   }
 
+  ngOnInit(): void {}
+
+  onSubmit() {
+    console.log(this.formNewPost.value);
+  }
 }
