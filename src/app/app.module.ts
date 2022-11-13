4ba8613d865846a480d84f6b58193d2c950c785a
Begocm <elcorreodebgo@gmail.com>
2022-11-13 20:03:23 +0100

    Install Bootstrap, components, interfaces, services and configure app-routing y app-module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListaPostsComponent } from './components/lista-posts/lista-posts.component';
+import { FormularioComponent } from './components/formulario/formulario.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, ListaPostsComponent, FormularioComponent],
+  imports: [BrowserModule, AppRoutingModule, ReactiveFormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/interfaces/post.interface.ts
@@ -0,0 +1,8 @@
+export interface Post {
+  titulo: string;
+  texto: string;
+  autor: string;
+  image: string;
+  fecha: Date;
+  categoria: string;
+}
